d94928f55a3b0fe867b51434a55e476a73b74df2
MariemAymen <mariem.gam23@gmail.com>
2024-01-09 09:34:04 +0100

    Une page web à partir d'une maquette

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ function fetchComponent(url, elementId) {
     fetch(url)
       .then(response => response.text())
       .then(data => {
-        document.getElementById(elementId).innerHTML = data;
-         if (elementId.indexOf('blog-') !== -1) {
+        if (document.getElementById(elementId)) {
+         document.getElementById(elementId).innerHTML = data;
+          if (elementId.indexOf('blog-') !== -1 && document.getElementById('card-img')) {
               document.getElementById('card-img').id = "card-img-"+ elementId
               document.getElementById("card-img-" + elementId).src = 'assets/images/'+ elementId + '.jpg'
-        }
-        if (elementId.indexOf('team-') !== -1) {
+           }
+          if (elementId.indexOf('team-') !== -1  && document.getElementById('card-ag-img')) {
               document.getElementById('card-ag-img').id =  "card-ag-img-" + elementId
               document.getElementById("card-ag-img-" + elementId).src = 'assets/images/'+ elementId + '.jpg'
-        } 
+          } 
+        }
       });
   }
